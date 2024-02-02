d2ad68abe77485b7021557d72c3fda5dcb8baf73
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2024-02-02 14:17:37 +0100

    Initializing Env

---
+++ src/main/java/tn/esprit/esprittn/EspritTnApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.esprittn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspritTnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspritTnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/esprittn/EspritTnApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.esprittn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspritTnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
